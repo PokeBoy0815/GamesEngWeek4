b6b1182d76e994cc45c611f8a0821fedbbbe123f
Markus Wei****ger <markus.weinberger@ur.de>
2024-08-28 10:47:46 +1000

    80 percent complete

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    //public GameObject obj;
+    
+    private int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i += 1;
+        Debug.Log(gameObject.name+":"+i);
+    }
+}
